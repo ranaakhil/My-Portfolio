a06b0d95ec619fb2e1989af057e012498dc4699d
Akhil Rana <ranaakhil078@gmail.com>
2023-01-02 16:28:37 +0530

    Some changes in Portfolio

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -16,23 +16,23 @@ function Services() {
             <ul className='service_list'>
               <li>
                 <BiCheck className='service_list-icon'/>
-                <p>Lorem, ipsum dolor sit amet consectetur elit</p>
+                <p>Bring more clarity to your flows with a strong visual identity.</p>
               </li>
               <li>
                 <BiCheck className='service_list-icon'/>
-                <p>Lorem, ipsum dolor sit amet consectetur elit</p>
+                <p>Use our UI to create an app or web page that's easy for your users to understand and identify.</p>
               </li>
               <li>
                 <BiCheck className='service_list-icon'/>
-                <p>Lorem, ipsum dolor sit amet consectetur elit</p>
+                <p>ui elements are easy to communicate and make it easy for the user. </p>
               </li>
               <li>
                 <BiCheck className='service_list-icon'/>
-                <p>Lorem, ipsum dolor sit amet consectetur elit</p>
+                <p>They should be focused on simple, clear, nice and appealing design.</p>
               </li>
               <li>
                 <BiCheck className='service_list-icon'/>
-                <p>Lorem, ipsum dolor sit amet consectetur elit</p>
+                <p>We’re very excited to share details of the new UI (User Interface), which we are rolling out to all users.</p>
               </li>
             </ul>
           </article>
@@ -47,27 +47,27 @@ function Services() {
             <ul className='service_list'>
               <li>
                 <BiCheck className='service_list-icon'/>
-                <p>Lorem, ipsum dolor sit amet consectetur elit</p>
+                <p>We are a web design team focused on effective and efficient website development</p>
               </li>
               <li>
                 <BiCheck className='service_list-icon'/>
-                <p>Lorem, ipsum dolor sit amet consectetur elit</p>
+                <p>developing with the most modern technologies. </p>
               </li>
               <li>
                 <BiCheck className='service_list-icon'/>
-                <p>Lorem, ipsum dolor sit amet consectetur elit</p>
+                <p>If you need a website or web design, please contact us. We can make any website according to your needs.</p>
               </li>
               <li>
                 <BiCheck className='service_list-icon'/>
-                <p>Lorem, ipsum dolor sit amet consectetur elit</p>
+                <p>Web Development includes but is not limited to: CSS, Javascript, Jquery, HTML and PHP.</p>
               </li>
               <li>
                 <BiCheck className='service_list-icon'/>
-                <p>Lorem, ipsum dolor sit amet consectetur elit</p>
+                <p>Power your web app with the most powerful and flexible Web toolset available.</p>
               </li>
               <li>
                 <BiCheck className='service_list-icon'/>
-                <p>Lorem, ipsum dolor sit amet consectetur elit</p>
+                <p>We have developed our own framework and cloud based CMS for our clients.</p>
               </li>
             </ul>
           </article>
@@ -82,23 +82,23 @@ function Services() {
             <ul className='service_list'>
               <li>
                 <BiCheck className='service_list-icon'/>
-                <p>Lorem, ipsum dolor sit amet consectetur elit</p>
+                <p>Create, share and display your media content in a format that suits your needs.</p>
               </li>
               <li>
                 <BiCheck className='service_list-icon'/>
-                <p>Lorem, ipsum dolor sit amet consectetur elit</p>
+                <p>Create high-impact, engaging content that converts visitors into customers.</p>
               </li>
               <li>
                 <BiCheck className='service_list-icon'/>
-                <p>Lorem, ipsum dolor sit amet consectetur elit</p>
+                <p>Create visually stunning content and share your passion with the world.</p>
               </li>
               <li>
                 <BiCheck className='service_list-icon'/>
-                <p>Lorem, ipsum dolor sit amet consectetur elit</p>
+                <p>A blog, a web page, or a channel for sharing content.</p>
               </li>
               <li>
                 <BiCheck className='service_list-icon'/>
-                <p>Lorem, ipsum dolor sit amet consectetur elit</p>
+                <p>Our content creation service provides new ways of thinking about how you do business.</p>
               </li>
             </ul>
           </article>
